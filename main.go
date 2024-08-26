040d9e5867e494d2c8d7bd569c0621bffb8048d9
Jean Rodrigo <jeanrodrigo_p@outlook.com>
2024-08-26 00:51:35 +0000

    Variaveis

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	titular := "Jean Rodrigo"
+	numeroAgencia := 589
+	numeroConta := 123456
+	saldo := 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	titular2 := Kemilly
+	numeroAgencia2 := 555
+	numeroConta2 := 111333
+	saldo2 := 200
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+}
