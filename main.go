e25e42479baa024533fac572f734ac41cf1bcc47
Jean Rodrigo <jeanrodrigo_p@outlook.com>
2024-08-26 01:52:07 +0000

    Metodo sacar

--- main.go
+++ main.go
@@ -12,5 +12,18 @@ type ContaCorrente struct {
 func main() {
 	contaDoJean := ContaCorrente{"Jean Rodrigo", 589, 123456, 125.5}
 
-	fmt.Println(contaDoJean)
+	fmt.Println(contaDoJean.saldo)
+
+	fmt.Println(contaDoJean.sacar(80))
+	fmt.Println(contaDoJean.saldo)
+}
+
+func (c *ContaCorrente) sacar(valorDoSaque float32) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insulficiente"
+	}
 }
